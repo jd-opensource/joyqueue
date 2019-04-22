efeb7d36b6958104b15647d51b3e1dd07193a28c
wangjin18 <wangjin18@jd.com>
2019-04-22 19:11:20 +0800

    sleep 300s

--- docker/bin/integrate_test.sh
+++ docker/bin/integrate_test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "integrate begin"
-sleep 60
-echo "finish test"
+echo "integrate begin" $(date "+%Y%m%d-%H%M%S")
+echo "pwd"
+sleep 300
+echo "finish test" $(date "+%Y%m%d-%H%M%S")
 exit 1
