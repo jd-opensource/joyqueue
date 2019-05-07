aaf16d43f20f6c4fc22c3038ce0bf96fef8bf112
wangxiaofei1 <wangxiaofei1@jd.com>
2019-05-07 11:28:58 +0800

    打印些日志

--- journalq-console/journalq-portal/src/views/topic/partitionGroup.vue
+++ journalq-console/journalq-portal/src/views/topic/partitionGroup.vue
@@ -144,7 +144,7 @@ export default {
           {
             title: '副本数',
             width: '2%',
-            key: 'replicas'
+            key: 'replicas.size'
           },
           {
             title: '当前leader',
