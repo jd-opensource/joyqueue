a48a3317b9e8b95201eea98d664e6c440330b3e4
liyue25 <liyue25@jd.com>
2019-05-10 18:55:01 +0800

    创建文件之前检查磁盘空间是否足够。

---
+++ journalq-server/journalq-store/journalq-store-core/src/main/java/com/jd/journalq/store/file/DiskFullException.java
@@ -0,0 +1,15 @@
+package com.jd.journalq.store.file;
+
+import java.io.File;
+
+/**
+ * @author liyue25
+ * Date: 2019-05-10
+ */
+public class DiskFullException extends RuntimeException {
+    private File file;
+    public DiskFullException(File file) {
+        super(String.format("No enough space on disk, create file %s failed!", file.getAbsolutePath()));
+        this.file = file;
+    }
+}
