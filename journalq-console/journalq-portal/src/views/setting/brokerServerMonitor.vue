8a268275a173708c463b602c8e1388d3ef558523
wangxiaofei1 <wangxiaofei1@jd.com>
2019-05-17 15:32:00 +0800

    调整页面宽度

--- journalq-console/journalq-portal/src/views/setting/brokerServerMonitor.vue
+++ journalq-console/journalq-portal/src/views/setting/brokerServerMonitor.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
-      <div style="height: 80px;">
-        <h4>连接数</h4>
-        <br>
-        <span>总连接数:</span><span>{{connection.total}}</span>
-        <span>消费者:</span><span>{{connection.consumer}}</span>
-        <span style="width: 10px"></span>
-        <span>生产者:</span><span>{{connection.producer}}</span>
-      </div>
-      <div>
-        <h4>存储监控</h4>
-        <br>
-        <span>存储监控:</span><span>{{store.start}}</span>
-        <span>总磁盘大小:</span><span>{{store.totalSpace | numFilter}}GB</span>
-        <span>空闲磁盘大小:</span><span>{{store.freeSpace | numFilter}}GB</span>
-      </div>
+    <div style="height: 60px;">
+      <h4>连接数</h4>
+      <br>
+      <span>总连接数:</span><span>{{detail.connection.total}}</span>
+      <span>消费者:</span><span>{{detail.connection.consumer}}</span>
+      <span style="width: 10px"></span>
+      <span>生产者:</span><span>{{detail.connection.producer}}</span>
+    </div>
+    <div>
+      <h4>存储监控</h4>
+      <br>
+      <span>状态:</span><span v-if="detail.store.started" style="background: #07bf0c">启动</span><span v-else style="background: #bf291e">未启动</span>
+      <span>总磁盘大小:</span><span>{{detail.store.totalSpace | numFilter}}GB</span>
+      <span>空闲磁盘大小:</span><span>{{detail.store.freeSpace | numFilter}}GB</span>
+    </div>
+    <div>
+      <h4>选举</h4>
+      <br>
+      <span>状态:</span><span v-if="detail.election.started" style="background: #07bf0c">启动</span><span v-else style="background: #bf0f20">未启动</span>
+    </div>
+    <div>
+      <h4>nameServer</h4>
+      <br>
+      <span>状态:</span><span v-if="detail.nameServer.started" style="background: #07bf0c">启动</span><span v-else style="background: #bf0f20">未启动</span>
+    </div>
   </div>
 </template>
 
@@ -33,20 +43,29 @@
         urls:{
           findbroker: '/monitor/broker'
         },
-        store:{
+        detail:{
+          store:{
 
+          },
+          connection:{
+
+          },
+          nameServer:{
+
+          },
+          election:{
+
+          }
         },
-        connection:{
 
-        }
       }
     },
     methods: {
       getList(){
         apiRequest.get(this.urls.findbroker+"/" + this.brokerId,"",{}).then(data=>{
           if(data.code == 200) {
-            this.store = data.data.store;
-            this.connection =  data.data.connection;
+            this.detail = data.data;
+            console.log(data.data)
         }
         })
       }
