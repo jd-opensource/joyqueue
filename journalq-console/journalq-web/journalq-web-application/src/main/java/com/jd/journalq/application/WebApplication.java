420fa793341e5b743e9bec0feb7b4bfdbe56b318
lindeqiang <lindeqiang@jd.com>
2019-06-03 14:15:27 +0800

    去除无用引入

--- journalq-console/journalq-web/journalq-web-application/src/main/java/com/jd/journalq/application/WebApplication.java
+++ journalq-console/journalq-web/journalq-web-application/src/main/java/com/jd/journalq/application/WebApplication.java
@@ -14,7 +14,6 @@
 package com.jd.journalq.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
