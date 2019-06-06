724ed792053a22922810d6e8b464804706dda1aa
gaohaoxiang <gaohaoxiang@jd.com>
2019-06-06 16:27:05 +0800

    merge

--- journalq-server/journalq-nsr/journalq-nsr-core/src/main/java/com/jd/journalq/nsr/network/NsrExceptionHandler.java
+++ journalq-server/journalq-nsr/journalq-nsr-core/src/main/java/com/jd/journalq/nsr/network/NsrExceptionHandler.java
@@ -16,14 +16,26 @@ package com.jd.journalq.nsr.network;
 import com.jd.journalq.network.transport.Transport;
 import com.jd.journalq.network.transport.command.Command;
 import com.jd.journalq.network.transport.command.handler.ExceptionHandler;
+import com.jd.journalq.network.transport.exception.TransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author wylixiaobin
  * Date: 2019/1/27
  */
 public class NsrExceptionHandler implements ExceptionHandler {
+
+    protected static final Logger logger = LoggerFactory.getLogger(NsrExceptionHandler.class);
+
     @Override
     public void handle(Transport transport, Command command, Throwable throwable) {
+        if (TransportException.isClosed(throwable)) {
+            logger.warn("channel close, address: {}, message: {}", throwable, throwable.getMessage());
+            return;
+        }
 
+        logger.error("nameserver exception, transport: {}, command: {}", transport, command, throwable);
     }
+
 }
