5bca38db7c5fb58111e7d6343f8e4ee7eff34006
chenyanying3 <chenyanying3@jd.com>
2019-07-02 11:01:40 +0800

    管理端portal自动编译报错

--- journalq-console/journalq-portal/gulpfile.js
+++ journalq-console/journalq-portal/gulpfile.js
@@ -17,9 +17,8 @@ var del = require('del');
 var runSequence = require('gulp-sequence');
 var htmlreplace = require('gulp-html-replace');
 
-
 gulp.task('journalq',function (cb) {
-    runSequence('cleanjournalq', 'movejournalq')(function () {
+    runSequence('clearWebroot', 'moveToWebroot')(function () {
         cb()
         console.log('journalq编译完毕');
     });
@@ -29,12 +28,13 @@ gulp.task('journalq',function (cb) {
  下面是子任务
  ----------------------------------------*/
 
-gulp.task('cleanjournalq', function (cb) {
+gulp.task('clearWebroot', function (cb) {
     var targetPath = path.resolve(__dirname, '../journalq-web/journalq-web-webroot/src/main/webroot');
-    console.log('清空 /journalq 目录');
+    console.log('clear /journalq 目录');
     return  del([targetPath+'/**/*'],{force:true});
 });
-gulp.task('movejournalq',function () {
+
+gulp.task('moveToWebroot',function () {
     console.log('journalq/dist和html到后端目录');
     gulp.src(['./dist/**','!./dist/index.html'])
         .pipe(gulp.dest(path.resolve(__dirname,  '../journalq-web/journalq-web-webroot/src/main/webroot')));
