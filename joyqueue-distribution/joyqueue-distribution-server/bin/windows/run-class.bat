569013c118962af57a6ad6c5347cbd69b5b8909f
gaohaoxiang <gaohaoxiang@jd.com>
2019-07-19 17:34:44 +0800

    merge

--- joyqueue-distribution/joyqueue-distribution-server/bin/windows/run-class.bat
+++ joyqueue-distribution/joyqueue-distribution-server/bin/windows/run-class.bat
@@ -15,7 +15,7 @@
 setlocal enabledelayedexpansion
 
 PUSHD %~dp0..\..
-SET APP_HOME=%CD%
+SET "APP_HOME=%CD%"
 POPD
 
 IF ["%JAVA_HOME%"] EQU [""] (
@@ -24,11 +24,10 @@ IF ["%JAVA_HOME%"] EQU [""] (
 	SET JAVA_CMD="%JAVA_HOME%\bin\java"
 )
 
-SET JAVA_OPTS="-server -Xms1024m -Xmx1024m -Xmn256m -Xss256k"
-SET "JAVA_OPTS="
-SET LIBPATH=%APP_HOME%\lib\*
-SET CLASSPATH=%APP_HOME%\conf
-SET LIB_CLASSPATH=%CLASSPATH%;%LIBPATH%
+SET "JAVA_OPTS=-server -Xms1024m -Xmx1024m -Xmn256m -Xss256k"
+SET LIBPATH="%APP_HOME%\lib\*"
+SET CLASSPATH="%APP_HOME%\conf"
+SET "LIB_CLASSPATH=%CLASSPATH%;%LIBPATH%"
 
 SET run=%JAVA_CMD% %JAVA_OPTS% -classpath %LIB_CLASSPATH% -Dfile.encoding=UTF-8 %*
 
