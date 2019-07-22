626d7a50f446c268d83794a464a5ff6f12f24acb
gaohaoxiang <gaohaoxiang@jd.com>
2019-07-22 15:15:30 +0800

    完善注释

--- joyqueue-server/joyqueue-broker-core/src/main/java/com/jd/joyqueue/broker/monitor/service/ConsumerMonitorService.java
+++ joyqueue-server/joyqueue-broker-core/src/main/java/com/jd/joyqueue/broker/monitor/service/ConsumerMonitorService.java
@@ -29,56 +29,58 @@ import java.util.List;
 public interface ConsumerMonitorService {
 
     /**
-     * 获取所有消费topic监控信息
+     * 获取所有消费监控信息
      *
-     * @return
+     * @param page 页数
+     * @param pageSize 每页数量
+     * @return 分页的消费监控信息
      */
     Pager<ConsumerMonitorInfo> getConsumerInfos(int page, int pageSize);
 
     /**
-     * 获取消费者信息
+     * 获取主题下应用的消费监控信息
      *
-     * @param topic
-     * @param app
-     * @return
+     * @param topic 主题
+     * @param app 应用
+     * @return 消费者监控信息
      */
     ConsumerMonitorInfo getConsumerInfoByTopicAndApp(String topic, String app);
 
     /**
-     * 获取消费者信息
+     * 获取主题下应用所有分区的消费监控信息
      *
-     * @param topic
-     * @param app
-     * @return
+     * @param topic 主题
+     * @param app 应用
+     * @return 消费监控信息列表
      */
     List<ConsumerPartitionMonitorInfo> getConsumerPartitionInfos(String topic, String app);
 
     /**
-     * 获取消费者信息
+     * 获取主题下应用分区的消费监控信息
      *
-     * @param topic
-     * @param app
-     * @param partition
-     * @return
+     * @param topic 主题
+     * @param app 应用
+     * @param partition 分区
+     * @return 消费监控信息
      */
     ConsumerPartitionMonitorInfo getConsumerPartitionInfoByTopicAndApp(String topic, String app, short partition);
 
     /**
-     * 获取消费者信息
+     * 获取主题下应用所有分区组的消费监控信息
      *
-     * @param topic
-     * @param app
-     * @return
+     * @param topic 主题
+     * @param app 应用
+     * @return 分区组消费监控信息列表
      */
     List<ConsumerPartitionGroupMonitorInfo> getConsumerPartitionGroupInfos(String topic, String app);
 
     /**
-     * 获取消费者信息
+     * 获取主题下应用分组的消费监控信息
      *
-     * @param topic
-     * @param app
-     * @param partitionGroupId
-     * @return
+     * @param topic 主题
+     * @param app 应用
+     * @param partitionGroupId 分区组
+     * @return 分区组消费监控信息
      */
     ConsumerPartitionGroupMonitorInfo getConsumerPartitionGroupInfoByTopicAndApp(String topic, String app, int partitionGroupId);
 
