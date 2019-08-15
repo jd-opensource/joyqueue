c79cd6469e0c43115d923303f9f9e8bb2c5081ff
gaohaoxiang <gaohaoxiang@jd.com>
2019-08-15 14:56:31 +0800

    去掉协议

--- joyqueue-console/joyqueue-portal/.eslintrc.js
+++ joyqueue-console/joyqueue-portal/.eslintrc.js
@@ -1,16 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
@@ -24,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
--- joyqueue-console/joyqueue-portal/src/mixins/detail.js
+++ joyqueue-console/joyqueue-portal/src/mixins/detail.js
@@ -1,16 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 // 增删改查方法，包括查询的getList, 分页的改变，以及新增，编辑，删除
 // 新增编辑都直接使用固定的弹框显示信息对象createDiag，同样的数据绑定对象createData，内容各不相同
 
