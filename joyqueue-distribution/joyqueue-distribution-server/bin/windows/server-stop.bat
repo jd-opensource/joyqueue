90b82767fb14422967b8e0e2ba1d676e50070b77
gaohaoxiang <gaohaoxiang@jd.com>
2019-08-15 15:17:19 +0800

    添加协议

--- joyqueue-distribution/joyqueue-distribution-server/bin/windows/server-stop.bat
+++ joyqueue-distribution/joyqueue-distribution-server/bin/windows/server-stop.bat
@@ -1,3 +1,19 @@
+@REM
+@REM Copyright 2018 The JoyQueue Authors.
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 
 wmic process where (commandline like "%%Launcher%%" and not name="wmic.exe") delete
 rem ps ax | grep -i 'Launcher' | grep -v grep | awk '{print $1}' | xargs kill -SIGTERM
