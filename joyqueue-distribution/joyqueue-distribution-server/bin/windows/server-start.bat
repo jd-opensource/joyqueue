598ddb454fbbe7d534ae2423ec8f7a4a7fead714
gaohaoxiang <gaohaoxiang@jd.com>
2019-08-15 15:19:55 +0800

    添加协议

--- joyqueue-distribution/joyqueue-distribution-server/bin/windows/server-start.bat
+++ joyqueue-distribution/joyqueue-distribution-server/bin/windows/server-start.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright 2018 The JoyQueue Authors.
+@REM Copyright 2019 The JoyQueue Authors.
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
