f71541730c6f6146e43d6cdc486ec8840af26d69
rudy2steiner <rudy_steiner@163.com>
2019-12-30 19:53:08 +0800

    File release test

---
+++ joyqueue-server/joyqueue-broker-core/src/test/java/org/joyqueue/broker/archive/FileReleaseTest.java
@@ -0,0 +1,55 @@
+package org.joyqueue.broker.archive;
+
+import org.joyqueue.toolkit.io.Files;
+import org.joyqueue.toolkit.time.SystemClock;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileReleaseTest {
+    String userPath = System.getProperty("java.io.tmpdir");
+    String baseDir=userPath+"/archive";
+    int  fileSize= 1024*1024*16;
+
+    @Test
+    public void releaseTest() throws Exception {
+
+        emptyDir(baseDir);
+        String fileName=String.valueOf(SystemClock.now());
+        File baseFile=new File(baseDir);
+        if(!baseFile.exists()){
+            baseFile.mkdirs();
+        }
+        File file=new File(baseDir,fileName);
+        System.out.println(file.getAbsolutePath());
+        RandomAccessFile raf=new RandomAccessFile(file,"rw");
+        FileChannel channel=raf.getChannel();
+        MappedByteBuffer buffer=channel.map(FileChannel.MapMode.READ_WRITE,0,fileSize);
+
+        Thread.sleep(10000);
+        // un close  and new another
+
+        fileName= String.valueOf(SystemClock.now());
+        file=new File(baseDir,fileName);
+        System.out.println(file.getAbsolutePath());
+        raf=new RandomAccessFile(file,"rw");
+        channel=raf.getChannel();
+        buffer=channel.map(FileChannel.MapMode.READ_WRITE,0,fileSize);
+        buffer.position(fileSize-1);
+        Thread.sleep(60000*10);
+    }
+
+
+    /**
+     * Empty directory
+     **/
+    public void emptyDir(String path){
+        File file=new File(path);
+        if(file.exists()){
+            Files.deleteDirectory(file);
+        }
+    }
+}
