ea4b639f48f0ecf76f734ed6e21ab3c5af9786f0
LiYue <liyue2008@gmail.com>
2020-01-08 09:21:44 +0800

    改进生产配置布局

--- joyqueue-console/joyqueue-portal/src/views/monitor/producerConfigForm.vue
+++ joyqueue-console/joyqueue-portal/src/views/monitor/producerConfigForm.vue
@@ -1,12 +1,18 @@
 <template>
-  <d-form ref="form" :model="formData" :rules="rules" label-width="100px" style="overflow-y:auto; width: 100%; padding-right: 20px">
-    <d-form-item label="就近机房发送:">
-      <d-switch v-model="formData.nearBy" :disabled="!nearbyConfigEnabled"></d-switch>
-    </d-form-item>
-    <d-form-item label="生产归档:" >
-      <d-switch v-model="formData.archive" :disabled="!archiveConfigEnabled"></d-switch>
-    </d-form-item>
-    <d-form-item label="限制IP生产:">
+  <d-form ref="form" :model="formData" :rules="rules" label-width="120px">
+    <grid-row>
+      <grid-col span="12">
+        <d-form-item label="就近机房发送">
+          <d-switch v-model="formData.nearBy" :disabled="!nearbyConfigEnabled"></d-switch>
+        </d-form-item>
+      </grid-col>
+      <grid-col span="12">
+        <d-form-item label="生产归档" >
+          <d-switch v-model="formData.archive" :disabled="!archiveConfigEnabled"></d-switch>
+        </d-form-item>
+      </grid-col>
+    </grid-row>
+    <d-form-item label="限制IP生产">
       <d-input type="textarea" rows="4" v-model="formData.blackList" placeholder="请输入要限制的IP，多个IP之间请用英文逗号隔开"/>
     </d-form-item>
   </d-form>
