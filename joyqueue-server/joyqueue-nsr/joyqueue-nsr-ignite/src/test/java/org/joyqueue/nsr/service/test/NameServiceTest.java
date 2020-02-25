f89e3e036ceda38df90d7d1a837fc7706824a719
gaohaoxiang <gaohaoxiang@jd.com>
2020-02-25 16:30:30 +0800

    元数据相关添加缓存和监控

--- joyqueue-server/joyqueue-nsr/joyqueue-nsr-ignite/src/test/java/org/joyqueue/nsr/service/test/NameServiceTest.java
+++ joyqueue-server/joyqueue-nsr/joyqueue-nsr-ignite/src/test/java/org/joyqueue/nsr/service/test/NameServiceTest.java
@@ -15,15 +15,6 @@
  */
 package org.joyqueue.nsr.service.test;
 
-import org.joyqueue.nsr.nameservice.NameServerInternal;
-import org.joyqueue.nsr.NameService;
-import org.joyqueue.nsr.nameservice.ThinNameService;
-import org.joyqueue.nsr.config.NameServiceConfig;
-import org.joyqueue.toolkit.config.PropertySupplier;
-import org.joyqueue.toolkit.config.PropertySupplierAware;
-import org.joyqueue.toolkit.io.Files;
-import org.joyqueue.toolkit.lang.Close;
-import org.joyqueue.toolkit.network.IpUtil;
 import org.joyqueue.domain.AppToken;
 import org.joyqueue.domain.Broker;
 import org.joyqueue.domain.ClientType;
@@ -34,10 +25,26 @@ import org.joyqueue.domain.Replica;
 import org.joyqueue.domain.Subscription;
 import org.joyqueue.domain.TopicConfig;
 import org.joyqueue.domain.TopicName;
-import org.junit.*;
+import org.joyqueue.nsr.NameService;
+import org.joyqueue.nsr.config.NameServiceConfig;
+import org.joyqueue.nsr.nameservice.NameServer;
+import org.joyqueue.nsr.nameservice.ThinNameService;
+import org.joyqueue.toolkit.config.PropertySupplier;
+import org.joyqueue.toolkit.config.PropertySupplierAware;
+import org.joyqueue.toolkit.io.Files;
+import org.joyqueue.toolkit.lang.Close;
+import org.joyqueue.toolkit.network.IpUtil;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.File;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author wylixiaobin
@@ -46,7 +53,7 @@ import java.util.*;
 
 public class NameServiceTest {
     private static NameService nameService;
-    private static NameServerInternal nameServer;
+    private static NameServer nameServer;
     private static File dataRoot = new File(System.getProperty("java.io.tmpdir") + "/joyqueue/nsr_temp");
 
     @BeforeClass
@@ -70,7 +77,7 @@ public class NameServiceTest {
 
         NameServiceConfig nameServiceConfig = new NameServiceConfig(new PropertySupplier.MapSupplier(properties));
 
-        nameServer = new NameServerInternal();
+        nameServer = new NameServer();
         nameServer.setSupplier(new PropertySupplier.MapSupplier(properties));
         nameServer.start();
 
