1b3245df8eeafdec6a1bf4013f507c5e3bc8f940
iamazy <1448588084@qq.com>
2020-04-05 20:54:03 +0800

    将Broker分组查询完全匹配改为模糊搜索

--- joyqueue-console/joyqueue-data/joyqueue-data-service/src/main/java/org/joyqueue/service/impl/BrokerServiceImpl.java
+++ joyqueue-console/joyqueue-data/joyqueue-data-service/src/main/java/org/joyqueue/service/impl/BrokerServiceImpl.java
@@ -37,7 +37,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;	import java.util.*;
+import java.util.Map;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
