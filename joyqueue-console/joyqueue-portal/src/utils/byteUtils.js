b6121739a0fb3a47bbde5b7eb134cea6855b81b5
iamazy <1448588084@qq.com>
2020-04-10 13:24:44 +0800

    Broker管理详情页添加重试数，重试tps和积压数

--- joyqueue-console/joyqueue-portal/src/utils/byteUtils.js
+++ joyqueue-console/joyqueue-portal/src/utils/byteUtils.js
@@ -1,6 +1,8 @@
-export default function bytesToSize (bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-  if (bytes === 0) return '0 Byte'
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+export default function bytesToSize (bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
