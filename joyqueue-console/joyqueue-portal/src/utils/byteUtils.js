86c385c97cf6681565d5678848888c156791f21d
iamazy <1448588084@qq.com>
2020-05-09 15:53:28 +0800

    管理端修改的一些小问题

--- joyqueue-console/joyqueue-portal/src/utils/byteUtils.js
+++ joyqueue-console/joyqueue-portal/src/utils/byteUtils.js
@@ -1,8 +1,8 @@
 export default function bytesToSize (bytes, decimals = 2) {
-  if (bytes === 0) return '0 Bytes'
+  if (bytes === 0) return '0 Bytes/s'
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const sizes = ['Bytes/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s', 'PB/s', 'EB/s', 'ZB/s', 'YB/s']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
