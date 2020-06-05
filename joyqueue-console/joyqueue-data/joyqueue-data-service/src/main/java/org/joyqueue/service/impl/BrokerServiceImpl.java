779905054a1246fcf21f32c1a51d4af90e1ba6f4
iamazy <1448588084@qq.com>
2020-06-05 16:35:42 +0800

    remove unused package

--- joyqueue-console/joyqueue-data/joyqueue-data-service/src/main/java/org/joyqueue/service/impl/BrokerServiceImpl.java
+++ joyqueue-console/joyqueue-data/joyqueue-data-service/src/main/java/org/joyqueue/service/impl/BrokerServiceImpl.java
@@ -41,7 +41,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
