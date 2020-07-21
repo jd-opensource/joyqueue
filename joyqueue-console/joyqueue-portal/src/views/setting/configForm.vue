b08ea458ccddbff31c230d52079252b29938f84f
Clinkzzzzzz <Clinkzzzzzz@users.noreply.github.com>
2020-07-21 13:08:09 +0800

    分组可以直接输入和下拉选择，可选all，override

--- joyqueue-console/joyqueue-portal/src/views/setting/configForm.vue
+++ joyqueue-console/joyqueue-portal/src/views/setting/configForm.vue
@@ -5,8 +5,24 @@
       <d-input v-model="formData.key" oninput="value = value.trim()" v-else></d-input>
     </d-form-item>
     <d-form-item label="分组:" placeholser="支持字母大小写、数字、. _ / -,首字母" prop="group" style="width: 60%">
-      <d-input v-model="formData.group" oninput="value = value.trim()" v-if="type==$store.getters.editFormType" disabled></d-input>
-      <d-input v-model="formData.group" oninput="value = value.trim()" v-else></d-input>
+      <d-autocomplete
+        class="inline-input"
+        v-model="formData.group"
+        :fetch-suggestions="querySearch"
+        placeholder="请输入内容"
+        @select="handleSelect"
+        v-if="type==$store.getters.editFormType" disabled
+        style="width: 100%"
+      ></d-autocomplete>
+      <d-autocomplete
+        class="inline-input"
+        v-model="formData.group"
+        :fetch-suggestions="querySearch"
+        placeholder="请输入内容"
+        @select="handleSelect"
+        v-else
+        style="width: 100%"
+      ></d-autocomplete>
     </d-form-item>
     <d-form-item label="值:" prop="value" style="width: 60%">
       <d-input v-model="formData.value" oninput="value = value.trim()"></d-input>
@@ -19,6 +35,18 @@ import form from '../../mixins/form.js'
 import {getCodeRule3} from '../../utils/common.js'
 
 export default {
+  data () {
+    return {
+      dataSources: [],
+      state1: '',
+      formData: this.data,
+      password: 'false',
+      rules: {
+        key: getCodeRule3(),
+        value: [{required: true, message: '请填写值', trigger: 'change'}]
+      }
+    }
+  },
   name: 'config-form',
   mixins: [ form ],
   props: {
@@ -36,20 +64,29 @@ export default {
     }
   },
   methods: {
-    getFormData () {
-      return this.formData
+    querySearch (queryString, cb) {
+      var dataSources = this.dataSources
+      var results = queryString ? dataSources.filter(this.createFilter(queryString)) : dataSources
+      // 调用 callback 返回建议列表的数据
+      cb(results)
+    },
+    createFilter (queryString) {
+      return (dataSource) => {
+        return (dataSource.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+      }
+    },
+    loadAll () {
+      return [
+        { 'value': 'all' },
+        { 'value': 'override' }
+      ]
+    },
+    handleSelect (item) {
+      console.log(item)
     }
   },
-  data () {
-    return {
-      formData: this.data,
-      password: 'false',
-      rules: {
-        key: getCodeRule3(),
-        value: [{required: true, message: '请填写值', trigger: 'change'}],
-        group: getCodeRule3()
-      }
-    }
+  mounted () {
+    this.dataSources = this.loadAll()
   }
 }
 </script>
