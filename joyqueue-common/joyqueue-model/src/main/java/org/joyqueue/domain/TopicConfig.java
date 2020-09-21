8e03d5a37e3c64c602ccdaf30d8d337e406d8f21
gaohaoxiang <gaohaoxiang@jd.com>
2020-09-21 17:45:49 +0800

    临时测试

--- joyqueue-common/joyqueue-model/src/main/java/org/joyqueue/domain/TopicConfig.java
+++ joyqueue-common/joyqueue-model/src/main/java/org/joyqueue/domain/TopicConfig.java
@@ -213,7 +213,7 @@ public class TopicConfig extends Topic implements Serializable {
             return true;
         }
 
-        return super.equals(((TopicConfig) o).getName());
+        return super.equals(o);
     }
 
     @Override
