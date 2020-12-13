e08d567baa4aa6212122eb3f1ad2df72b5bfb608
majun87 <majun8@jd.com>
2020-12-13 17:24:16 +0800

    增加测试覆盖率

--- joyqueue-server/joyqueue-broker-core/src/test/java/org/joyqueue/broker/archive/ConsumeArchiveServiceTest.java
+++ joyqueue-server/joyqueue-broker-core/src/test/java/org/joyqueue/broker/archive/ConsumeArchiveServiceTest.java
@@ -257,7 +257,7 @@ public class ConsumeArchiveServiceTest {
         consumeService.readAndWrite();
     }
 
-    //@Test
+    @Test
     public void testArchive() {
         ConsumeArchiveService.ArchiveMappedFileRepository archiveMappedFileRepository =
                 consumeService.new ArchiveMappedFileRepository(getTestPath());
